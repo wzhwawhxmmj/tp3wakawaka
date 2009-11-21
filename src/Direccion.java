0bcf97bc4e53ee182950d7a6a852e1571d2d2e42
smesel <smesel@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-11-21 02:08:52 +0000

    Primer commit de las clases que me corresponden. La razón de armar las clases Direccion, Jugador y Pacman es porque requería que estuvieran para que las pruebas funcionen.

---
+++ src/Direccion.java
@@ -0,0 +1,7 @@
+
+public class Direccion {
+	
+	public void derecha(){
+		
+	}
+}
---
+++ src/Pacman.java
@@ -0,0 +1,8 @@
+
+public class Pacman extends Jugador{
+
+	public Pacman(Punto p) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
