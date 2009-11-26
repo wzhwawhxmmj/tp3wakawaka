10cd91817e3df97bf63925259b18100cd2cdb7c2
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-11-26 00:08:38 +0000

    sigo con Juego.java

--- src/Juego.java
+++ src/Juego.java
@@ -1,4 +1,23 @@
 
 public class Juego {
-
+	
+	private Pacman pacman;
+	private Fantasma[] arrayDeFantasmas;
+	private Escenario escenario;
+	private long int puntaje;
+	private int vidas;
+	private int nivelActual;
+	//A verificar 
+	//Private bool frutaComida;
+	
+	public void inicializar (){
+		
+		this.pacman = new Pacman;
+		this.arrayDeFantasmas = new Fantasma[4];
+		this.escenario = new Escenario;
+		this.puntaje = 0;
+		this.vidas = 3;
+		this.nivelActual = 1;
+		
+	}
 }
