deebee7a0a3df4dc3c8b14029588f833b377a665
nicolascoco85 <nicolascoco85@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-11-27 14:17:14 +0000

    Hice un metodo cargar tablero bastante precario, pero no escapa mucho al que hace el verdadero, solo hay que instanciarle las cosas que guarda cada casillero y guardar el casillero en una posicion que querramos, aunque el while quiza no sea lo mejor. Escucho opiniones, desde aqui eso fue todo mi pobre conocimiento:(

--- src/Escenario.java
+++ src/Escenario.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-public class Escenario {/* creando el escenario, nada raro */
+public class Escenario {private static final Casillero Casillero = null;
+/* creando el escenario, nada raro */
 
 	private HashMap <Punto, Casillero>  tablero;
 
@@ -17,6 +18,27 @@ public class Escenario {/* creando el escenario, nada raro */
 
 		return  this.tablero.get(p);
 
+	}
+	
+	public void CargarTablero(){
+		/* falta cargar de alguna manera el casillero con
+		 * fruta, pildora o punto y fantasma para cada posicion*/
+		Punto p = new Punto(1, 1);
+		Casillero c = new Casillero();
+		int i= 1;
+		int j=1;
+				
+		while (p.getx()<51){
+			
+			while (p.gety()<51){
+				ponerEnPosicion(p,c);
+				p.sety(j);
+				j++;
+			}
+			p.setx(i);
+			i++;
+		}
+		
 	}
 
 }
