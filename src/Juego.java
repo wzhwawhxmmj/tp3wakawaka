5a0794cc86842d88c102ea2066a7e99a0a2bdee8
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-11-28 22:09:46 +0000

    Sigo con la implementación de la clase Juego.java

--- src/Juego.java
+++ src/Juego.java
@@ -4,7 +4,7 @@ public class Juego {
 	private Pacman pacman;
 	private Fantasma[] arrayDeFantasmas;
 	private Escenario escenario;
-	private long int puntaje;
+	private long puntaje;
 	private int vidas;
 	private int nivelActual;
 	//A verificar 
@@ -13,17 +13,18 @@ public class Juego {
 	
 	private void inicializarFantasmas(){
 		
-		arrayDeFantasmas[0] = new FantasmaRojo;
-		arrayDeFantasmas[1] = new FantasmaCeleste;
-		arrayDeFantasmas[2] = new FantasmaRosa;
-		arrayDeFantasmas[3] = new FantasmaNaranja;
+		arrayDeFantasmas[0] = new FantasmaRojo(escenario, escenario.getPosicionCasa());
+		arrayDeFantasmas[1] = new FantasmaCeleste(escenario, escenario.getPosicionCasa());
+		arrayDeFantasmas[2] = new FantasmaRosa(escenario, escenario.getPosicionCasa());
+		arrayDeFantasmas[3] = new FantasmaNaranja(escenario, escenario.getPosicionCasa());
 	}
 	
-	public void inicializar (){
+	public void inicializarNivel (int nivel){
 		
-		this.pacman = new Pacman;
+		
+		this.pacman = new Pacman(escenario, escenario.getPosicionInicialPacman());
 		this.arrayDeFantasmas = new Fantasma[4];
-		this.escenario = new Escenario;
+		this.escenario = new Escenario();
 		this.puntaje = 0;
 		this.vidas = 3;
 		this.nivelActual = 1;
