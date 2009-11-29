6c7c7c767a16bebe7eec8c0d30a1b12f76237ec1
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-11-29 19:14:21 +0000

    Hice un par de tests, al fin =P

---
+++ trunk/src/EntidadTest.java
@@ -0,0 +1,16 @@
+import junit.framework.TestCase;
+
+
+public class EntidadTest extends TestCase {
+
+	public void testEstanEnElMismoLado(){
+		Posicion pos = new Posicion(10,10);
+		Fantasma fantasmin = new FantasmaRojo(null, pos, 1, 0);
+		Pacman waka = new Pacman(null, pos, 1);
+		if(fantasmin.getPosicion().equals(waka.getPosicion())){
+			assert (true);
+		}
+		else fail();
+	 }
+	
+}
