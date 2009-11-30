821f4e058b2171b4cd4557340aabb0ef410da92f
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-11-30 03:29:18 +0000

    Modificación de la ListaDeEscenarios

--- trunk/src/ListaDeEscenarios.java
+++ trunk/src/ListaDeEscenarios.java
@@ -2,10 +2,34 @@
 public class ListaDeEscenarios {
 
 	private Collection<Escenario> listaDeEscenarios;
+	private int nivelActual;
+	
 	
 	public ListaDeEscenarios (){
 		this.listaDeEscenarios = new List<Escenario>();
-		
+		this.nivelActual = 0;
 	}
 	
+	public void cargarEscenarios (){
+		/*Acá se cargan los escenarios de Coco
+		 * 
+		*/
+	}
+	
+	public Escenario getEscenario(int nivel){
+		this.nivelActual = nivel;
+		return this.listaDeEscenarios.get(nivel-1);
+	}
+
+	
+	public boolean tieneSiguiente (){
+		try{
+			his.listaDeEscenarios.get(this.nivelActual)
+			return true;
+		}
+		catch (IndexOutOfBoundsException exception){
+			return false;
+		}
+		
+	}
 }
