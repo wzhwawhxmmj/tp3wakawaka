71dcdad4d7c612f3d44fe6bc29447db7ef4a56a9
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-11-30 11:41:40 +0000

    Posicion  * Ahora avanzarDerecha, arriba, etc  retornan tipo Posicion Utilitario  * Intentando hacer la recursividad, no falta mucho  Despues hay que ver como hacer el test.

--- src/Utilitario.java
+++ src/Utilitario.java
@@ -5,19 +5,31 @@ public class Utilitario {
 	public Utilitario(){
 	}
 	
-	public void caminoMasCorto(Escenario escenario, Posicion salida, Posicion llegada){
+	public static Direccion caminoMasCorto(Escenario escenario, Posicion salida, Posicion llegada){
 		Ueb aux;
-		HashMap arbol = new HashMap();
-		
-		
+		Direccion direccionARetornar = Direccion.NINGUNA;
+		int numeroDePasadas = 0;
+				
+		if(!escenario.sacarEnPosicion(salida).isPisablePorIA())
+			throw new PosicionIlegalException();
+
 		aux = escenario.sacarEnPosicion(salida);
-		
-		if (aux.isPisablePorIA() && aux.isPisablePorJugador()) return;
-		if (salida == llegada) 
-		
-		
-		
-		
-		
+		if (aux.isPisablePorIA()){ 
+			 Utilitario.caminoMasCorto(escenario, salida.avanzarArriba(), llegada);
+		     salida.avanzarAbajo();
+			 Utilitario.caminoMasCorto(escenario, salida.avanzarAbajo(), llegada);
+			 salida.avanzarArriba();
+			 Utilitario.caminoMasCorto(escenario, salida.avanzarDerecha(), llegada);
+			 salida.avanzarIzquierda();
+			 Utilitario.caminoMasCorto(escenario, salida.avanzarIzquierda(), llegada);
+			 salida.avanzarDerecha();
+			 if (salida == llegada){ 
+			 //direccionARetornar = ???;  	 
+		     }
+		}  
+		return direccionARetornar;
 	}
+
+
+
 }
