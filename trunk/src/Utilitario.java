5a2353cf8dbdd219c74057b33fbf4998059d9884
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-11-30 21:11:14 +0000

    Utilitario CaminoMasCorto Ahi va queriendo, pero tengo un problema, el tipo int pasa por valor y no puedo recordar el valor a lo largo de las iteraciones, y la clase integer no me permite modificar su valor :S

--- trunk/src/Utilitario.java
+++ trunk/src/Utilitario.java
@@ -1,39 +1,64 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Utilitario {
 	
 	public Utilitario(){
 	}
 	
-	private static Direccion iteracionCaminoMasCorto(Escenario escenario, Posicion salida, Posicion llegada, int numeroDePasadas,Direccion mejorDireccion){
+	private static void iteracionCaminoMasCorto(Escenario escenario, Posicion salida, Posicion llegada, int numeroDePasadas,int menorCantidad){
 		
 		if (escenario.sacarEnPosicion(salida).isPisablePorIA()){
 			if (salida.equals(llegada)){ 
-				 
+                  if(numeroDePasadas < menorCantidad){
+                	  menorCantidad = numeroDePasadas;
+                  }
 			     }
-			 Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarArriba()  ,llegada, numeroDePasadas + 1, mejorDireccion);
-		       salida.avanzarAbajo();
-			 Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarAbajo()   ,llegada, numeroDePasadas + 1, mejorDireccion);
-			   salida.avanzarArriba();
-			 Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarDerecha() ,llegada, numeroDePasadas + 1, mejorDireccion);
-			   salida.avanzarIzquierda();
-			 Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarIzquierda(),llegada,numeroDePasadas + 1, mejorDireccion);
-			   salida.avanzarDerecha();
-
+			else{
+				Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarArriba()  ,llegada, numeroDePasadas + 1, menorCantidad);
+				salida.avanzarAbajo();
+				Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarAbajo()   ,llegada, numeroDePasadas + 1, menorCantidad);
+				salida.avanzarArriba();
+				Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarDerecha() ,llegada, numeroDePasadas + 1, menorCantidad);
+				salida.avanzarIzquierda();
+				Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarIzquierda(),llegada,numeroDePasadas + 1, menorCantidad);
+				salida.avanzarDerecha();}
 	    }
-		return mejorDireccion;
 	}	
 	  	
 	
 	public static Direccion caminoMasCorto(Escenario escenario, Posicion salida, Posicion llegada){
-
-		Direccion direccionProxima = Direccion.NINGUNA; 
+		
+		final Direccion IntADireccion[] = {Direccion.ARRIBA, Direccion.ABAJO, Direccion.DERECHA, Direccion.IZQUIERDA};
+		final int MAXDirecciones = IntADireccion.length;
+		Integer cantidad[] = new Integer[MAXDirecciones];
+		
+		final int UnaCantidadDePasosExagerada = 999;
 		int numeroDePasadas = 0;
+		
+		int mejorCantidad;
+		int mejorDireccionEnInt;
 		
 		if(!escenario.sacarEnPosicion(salida).isPisablePorIA())
 			throw new PosicionIlegalException();
-
-		return iteracionCaminoMasCorto(escenario, salida, llegada, numeroDePasadas, direccionProxima);
-	}
-
-
-
+		
+		Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarArriba()  ,llegada, numeroDePasadas + 1, cantidad[0]);
+	       salida.avanzarAbajo();
+		Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarAbajo()   ,llegada, numeroDePasadas + 1, cantidad[1]);
+		   salida.avanzarArriba();
+		Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarDerecha() ,llegada, numeroDePasadas + 1, cantidad[2]);
+		   salida.avanzarIzquierda();
+		Utilitario.iteracionCaminoMasCorto(escenario, salida.avanzarIzquierda(),llegada,numeroDePasadas + 1, cantidad[3]);
+		   salida.avanzarDerecha();
+		   
+		mejorCantidad = UnaCantidadDePasosExagerada;
+		mejorDireccionEnInt = -1;
+		for(int i=0; i<MAXDirecciones; i++){
+			if(mejorCantidad > cantidad[i].intValue()){
+			   mejorCantidad = cantidad[i].intValue();
+			   mejorDireccionEnInt = i;
+			   }
+			}
+		return IntADireccion[mejorDireccionEnInt];
+		}
 }
