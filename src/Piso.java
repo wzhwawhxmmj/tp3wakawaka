a07bf44c1f1af1c9263a9e0575e2ac6885c79796
nicolascoco85 <nicolascoco85@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-02 20:07:45 +0000

    Se agragaron metodos en piso, y dejo de llamarse casillero. Cada vez que se quiera agregar un Nojugador a el piso este tendra un metodo distinto, que instanciara ese objeto. Ej: si quiero poner un punto, existe el metodo ponerPunto; este lo instanciara y lo agrega en el piso en la posicion indicada.

--- src/Piso.java
+++ src/Piso.java
@@ -1,33 +1,77 @@
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+
 public class Piso extends Ueb {
+	/*Se rellenan con cosas No jugador, fruta,puntito, pildora y fantasma*/
+	private List<NoJugador> casillero;
+	
 
 	public Piso() {
+
+		casillero = new ArrayList<NoJugador>();
 		this.setPisablePorIA(true);
 		this.setPisablePorJugador(true);
 
+	
 	}
-
-	public void ponerPunto(Punto p) {
-
-	}
-
-	public void ponerPildora(Punto p) {
-
-	}
-
-	public void ponerFruta(Punto p) {
+	public void ponerPunto(Posicion p) {
+		Puntito puntito = new Puntito(p,10);
+		casillero.add(puntito);
 
 	}
 
-	public void sacarPunto(Punto p) {
-
+	public void ponerPildora(Posicion p) {
+		Pildora pildora = new Pildora(p,30);
+		casillero.add(pildora);
+		
 	}
 
-	public void sacarPildora(Punto p) {
-
+	public void ponerFruta(Posicion p) {
+		Fruta fruta = new Fruta (p,100);
+		casillero.add(fruta);
 	}
 
-	public void sacarFruta(Punto p) {
+	
+	public void ponerFantasma(Posicion p,Fantasma fantasma) {
+		fantasma.setPosicion(p);
+		casillero.add(fantasma);
 
 	}
-
+	
+	public int cantidadDeComestibles(){
+		
+		return casillero.size();
+	}
+	
+	public NoJugador sacarComestible(int i){
+		/*Lo usaremos para vaciar la lista cuando el pacman pase
+		 * por aqui.
+		 */
+		
+		NoJugador comestible;
+		
+		comestible = casillero.get(i);
+		casillero.remove(i);
+		return comestible;
+	
+		
+	}
+	
+	public NoJugador consultarComestible(int i){
+	/* devuelve una el comestible en la en i sin borrarlo de
+	 * de la lista	
+	 */
+		return casillero.get(i);
+	}
+	
+	public void vaciarCasillero(){
+	
+		casillero.clear();
+	}
+	
+	
+		
 }
