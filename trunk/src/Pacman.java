85024fe7125a540463033a98d1a4e0c84532a30e
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-03 15:07:58 +0000

    Pacman, agrege un geter de la direccion, información útil para los fantamas

--- trunk/src/Pacman.java
+++ trunk/src/Pacman.java
@@ -12,7 +12,9 @@ public class Pacman extends Jugador {
 		algunaCosa.actuar();
 	}
 
-
+	public Direccion Direccion(){
+		return direccion;
+	}
 
 	public void cambiarDireccion(Direccion direccion){
 		this.direccion = direccion;
