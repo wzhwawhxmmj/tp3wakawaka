b9eb58dccd19b4b0d05e0fb1c87f03832645de01
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-04 19:53:47 +0000

    FantasmaRosa *Cambie las llamadas al escenario, ahora pide iteradores

--- src/FantasmaRosa.java
+++ src/FantasmaRosa.java
@@ -10,18 +10,20 @@ public class FantasmaRosa extends Fantasma {
 	public void estrategizar() {
 		// TODO Auto-generated method stub
 		Escenario escenario = this.getEscenario();
-		Iterator<NoJugador> iterador;
+		Iterator<NoJugador> iteradorCosas;
+		Iterator<Ueb> iteradorCasilleros;
 		NoJugador NoJugadorActual;
+		Ueb casillero;
 		Utilitario calculador;
 		int Xtotal,Ytotal;
 		int cantidad;
 		
-		for(int x;x<=escenario.getDimensionX();x++)
-			for(int y;y<=escenario.getDimensionY();y++)
-			    {
-			    iterador = escenario.sacarEnPosicion(new Posicion(x,y)).getArray().iterator();
-		        while(iterador.hasNext()){
-		        	NoJugadorActual = iterador.next();
+		iteradorCasilleros = escenario.getIterador();
+        while(iteradorCasilleros.hasNext()){
+		        casillero = iteradorCasilleros.next();	    
+			    iteradorCosas = casillero.getIterador();
+		        while(iteradorCosas.hasNext()){
+		        	NoJugadorActual = iteradorCosas.next();
 		        	Xtotal += NoJugadorActual.getPosicion().getx();
 		            Ytotal += NoJugadorActual.getPosicion().getx();
 		            cantidad++;
