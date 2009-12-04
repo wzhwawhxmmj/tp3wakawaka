72e9a7b86ef663e2f50c193823537b935524aa01
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-04 18:22:21 +0000

    Fantasma Rosa *Intente hacer algo, je

--- src/FantasmaRosa.java
+++ src/FantasmaRosa.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class FantasmaRosa extends Fantasma {
 
 	public FantasmaRosa(Escenario escenario, Posicion pos, int velocidad) {
@@ -6,6 +8,33 @@ public class FantasmaRosa extends Fantasma {
 	
 	@Override
 	public void estrategizar() {
+		// TODO Auto-generated method stub
+		Escenario escenario = this.getEscenario();
+		Iterator<NoJugador> iterador;
+		NoJugador NoJugadorActual;
+		Utilitario calculador;
+		int Xtotal,Ytotal;
+		int cantidad;
+		
+		for(int x;x<=escenario.getDimensionX();x++)
+			for(int y;y<=escenario.getDimensionY();y++)
+			    {
+			    iterador = escenario.sacarEnPosicion(new Posicion(x,y)).getArray().iterator();
+		        while(iterador.hasNext()){
+		        	NoJugadorActual = iterador.next();
+		        	Xtotal += NoJugadorActual.getPosicion().getx();
+		            Ytotal += NoJugadorActual.getPosicion().getx();
+		            cantidad++;
+		            }
+			    }
+		
+		calculador = escenario.getCalculador();
+		calculador.DireccionHaciaMenorCaminoEntre(this.getPosicion(), new Posicion(Xtotal/cantidad, Ytotal/cantidad));
+		
+	}
+
+	@Override
+	public void actuar() {
 		// TODO Auto-generated method stub
 		
 	}
