6d367721e6c75f92eca52aaa538fa2d6ca86735e
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-05 12:27:37 +0000

    Pacman,  toma sampy, ahi tenes lo de los puntos del pacman =P

--- src/Pacman.java
+++ src/Pacman.java
@@ -1,15 +1,18 @@
 public class Pacman extends Jugador {
-
-	Direccion direccion;
+	
+	private int puntajeAcumulado;
+	private Direccion direccion;
 
 	public Pacman(Escenario escenario, Posicion posicion, int velocidad) {
 		super(escenario, posicion, velocidad);
 		this.direccion = Direccion.NINGUNA;
+		puntajeAcumulado = 0;
 	}
 
 	public void comer(NoJugador algunaCosa) {
 		// Se supone que esta entidad es una fruta o punto amarillo
 		algunaCosa.actuar();
+		puntajeAcumulado += algunaCosa.getPuntos();
 	}
 
 	public Direccion Direccion(){
@@ -24,4 +27,9 @@ public class Pacman extends Jugador {
 		this.moverHacia(direccion);
 	}
 
+	public void getPuntajeAcumulado() {
+		return puntajeAcumulado;
+		
+	}
+	
 }
