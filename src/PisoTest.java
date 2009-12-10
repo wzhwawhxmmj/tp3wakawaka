f990e44a0b6cd99ac28f1cd8d569158891ef7c44
nicolascoco85 <nicolascoco85@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-10 23:54:01 +0000

    Se agregaron test de escenario y de piso

--- src/PisoTest.java
+++ src/PisoTest.java
@@ -13,5 +13,40 @@ public class PisoTest extends TestCase{
 		assertEquals(ueb.cantidadDeComestibles()==1, true);
 		
 	}
+	
+	public void testCargarPildoraAlPiso(){
+		Pildora pildora = new Pildora(new Posicion(1,2), 30);
+		Ueb ueb = new Piso();
+		
+		ueb.ponerNoJugador(pildora);
+		
+		assertEquals(ueb.sacarComestible(0).equals(pildora), true);
+		
+	}
+	
+	public void testCargarDosObjetosAlPiso(){
+		Pildora pildora = new Pildora(new Posicion(1,2), 30);
+		Fruta fruta = new Fruta ( new Posicion (1,2),100);
+		Ueb ueb = new Piso();
+		
+		ueb.ponerNoJugador(pildora);
+		ueb.ponerNoJugador(fruta);
+		
+		assertEquals(ueb.cantidadDeComestibles()==2, true);
+	}
+	
+	public void testPisovacio(){
+		Pildora pildora = new Pildora(new Posicion(1,2), 30);
+		Fruta fruta = new Fruta ( new Posicion (1,2),100);
+		Ueb ueb = new Piso();
+		
+		ueb.ponerNoJugador(pildora);
+		ueb.ponerNoJugador(fruta);
+		
+		ueb.vaciarCasillero();
+		
+		assertEquals(ueb.cantidadDeComestibles()==0, true);
+		
+	}
 
 }
