6204129222e5a46b06749d1cf039126d71878a5b
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-12 08:43:09 +0000

    Avance de mi fantasma Naranja.

--- trunk/src/FantasmaNaranja.java
+++ trunk/src/FantasmaNaranja.java
@@ -1,17 +1,56 @@
 /*Yo, Javier Sampietro, me encago del Fantasma Naranja*/
-
+import java.util.*;
+import java.util.Random;
 
 public class FantasmaNaranja extends Fantasma {
 
+	boolean arrasando;
+	int casillasArrasadas;
+	
 	public FantasmaNaranja(Escenario escenario, Posicion posInicial, Posicion posModoSeparacion , float velocidad, int puntosAlSerComido) {
 		super(escenario, posInicial, posModoSeparacion, velocidad, puntosAlSerComido);
-	}
+		this.arrasando = false;
+		this.casillasArrasadas = 0;
 	}
 
 
 	public void estrategizar() {
-		// TODO Auto-generated method stub
-		
+		if (this.arrasando)
+			this.arrasar();
+		else{
+			random = new Random();
+			random.nextInt (10);
+			if ((random==1)&& (this.casillasArrasadas < 10)){
+				this.arrasando = true;
+				this.arrasar();	
+			}
+			else
+				this.perseguir();
+		}
 	}
 
+	private void arrasar(){
+		Ueb casilleroActual;
+		NoJugador comestible;
+		if (this.casillasArrasadas >= 10){
+			this.arrasando = false;
+			return;
+		}
+		else{	
+			this.movimientoAlAzar();
+			casilleroActual = this.escenario.sacarEnPosicion(this.getPosicion());
+			casilleroActual.vaciarCasillero();
+			/* El tema de acá es ver bien como manejar el tema de los puntos
+			 * de las píldoras, y el pacman  debe chequear primero la casilla
+			 * porque sino puede pasar algo muy loco, el fantasma saca todo de la 
+			 * lista y el pacman no tienen nada que activar.
+			 */
+			this.casillasArrasadas = this.casillasArrasadas + 1;
+		}
+
+	}
+	
+	private void perseguir (){
+		/*A continuar*/
+	}
 }
