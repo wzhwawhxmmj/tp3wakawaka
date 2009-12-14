ff4988013230c8353e33fb6459b8f3210293de0b
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-14 15:20:59 +0000

    Pacman: Tests Agregados y Modificados para usar el escenario simple

--- src/PacmanTest.java
+++ src/PacmanTest.java
@@ -2,16 +2,58 @@ import junit.framework.TestCase;
 
 
 public class PacmanTest extends TestCase{
-	public void testSeMovio(){
-		Posicion pos = new Posicion(10,10);
-		Pacman waka = new Pacman(null, pos, 1);
+	public Escenario escenarioSimple(){
+		ListaDeEscenarios lista = new ListaDeEscenarios(); 
+		lista.cargarEscenarios();
+		return lista.getEscenario(1); 
+		}
+	
+	public void testSeMovioDerecha(){
+		Posicion pos = new Posicion(1,1);
+		Pacman waka = new Pacman(escenarioSimple(), pos, 1);
+		waka.cambiarDireccion(Direccion.DERECHA);
+		waka.vivir();
+		if(waka.getPosicion().equals(new Posicion(2,1))){
+			assert (true);
+			}
+		else fail();
+	 }
+
+	public void testSeMovioPared(){
+		Posicion pos = new Posicion(1,1);
+		Pacman waka = new Pacman(escenarioSimple(), pos, 1);
 		waka.cambiarDireccion(Direccion.ARRIBA);
 		waka.vivir();
-		if(waka.getPosicion().gety() == 11){
+		if(waka.getPosicion().equals(new Posicion(1,1))){
 			assert (true);
-		}
+			}
 		else fail();
 	 }
 	
+	public void testSeMovioAbajoyChoco(){
+		Posicion pos = new Posicion(1,1);
+		Pacman waka = new Pacman(escenarioSimple(), pos, 1);
+		waka.cambiarDireccion(Direccion.ABAJO);
+		waka.vivir();
+		waka.vivir();
+		waka.vivir();
+		waka.vivir();
+		if(waka.getPosicion().equals(new Posicion(1,3))){
+			assert (true);
+			}
+		else fail();
+	 }
 	
+	public void testSeMovioParedMedio(){
+		Posicion pos = new Posicion(3,3);
+		Pacman waka = new Pacman(escenarioSimple(), pos, 1);
+		waka.cambiarDireccion(Direccion.ARRIBA);
+		waka.vivir();
+		waka.cambiarDireccion(Direccion.IZQUIERDA);
+		waka.vivir();
+		if(waka.getPosicion().equals(new Posicion(3,2))){
+			assert (true);
+			}
+		else fail();
+	    }
 }
