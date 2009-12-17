554ea98d32eb7a370b384306440531c47c3a8bda
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-17 11:21:18 +0000

    Vistas varias.

---
+++ src/vista/VistaPacman.java
@@ -0,0 +1,13 @@
+package vista;
+
+import java.awt.Color;
+
+public class VistaPacman extends ar.uba.fi.algo3.titiritero.vista.Circulo {
+	 
+	        public VistaPacman() {
+	                super(5);
+	                setColor(Color.YELLOW);
+	        }
+
+
+}
---
+++ src/vista/VistaPiso.java
@@ -0,0 +1,13 @@
+package vista;
+
+import java.awt.Color;
+
+public class VistaPiso extends ar.uba.fi.algo3.titiritero.vista.Cuadrado{
+
+	public VistaPiso() {
+        super(8,8);
+        setColor(Color.GRAY);
+}
+
+
+}
