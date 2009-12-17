a5f66a45627cb64c2a920159ca053c0aed05b8b1
smesel <smesel@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-17 11:15:17 +0000

    Muchos cambios geniales.

---
+++ trunk/src/vista/VistaFantasmaRojo.java
@@ -0,0 +1,13 @@
+package vista;
+
+import java.awt.Color;
+import ar.uba.fi.algo3.titiritero.vista.Cuadrado;
+
+public class VistaFantasmaRojo extends Cuadrado {
+ 
+	public VistaFantasmaRojo() {
+		super(5,5);
+		setColor(Color.RED);
+	}
+
+}
---
+++ trunk/src/vista/VistaFantasmaRosa.java
@@ -0,0 +1,13 @@
+package vista;
+
+import java.awt.Color;
+import ar.uba.fi.algo3.titiritero.vista.Cuadrado;
+
+public class VistaFantasmaRosa extends Cuadrado {
+ 
+	public VistaFantasmaRosa() {
+		super(5,5);
+		setColor(Color.PINK);
+	}
+
+}
