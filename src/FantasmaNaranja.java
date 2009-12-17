a8c19c1ce2df71b773379d420276dbae26c4a3d1
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-17 02:42:06 +0000

    Finalizé con FantasmaNaranja, falta agregar un par de métodos a otras clases para que ande, ahora lo hago.

--- src/FantasmaNaranja.java
+++ src/FantasmaNaranja.java
@@ -1,25 +1,28 @@
 /*Yo, Javier Sampietro, me encago del Fantasma Naranja*/
 import java.util.*;
-import java.util.Random;
 
 public class FantasmaNaranja extends Fantasma {
 
 	boolean arrasando;
 	int casillasArrasadas;
 	
-	public FantasmaNaranja(Escenario escenario, Posicion posInicial, Posicion posModoSeparacion , float velocidad, int puntosAlSerComido) {
-		super(escenario, posInicial, posModoSeparacion, velocidad, puntosAlSerComido);
+	public FantasmaNaranja(Escenario escenario, Posicion posModoSeparacion , float duracionModoAzul, float velocidad, int puntosAlSerComido) {
+
+		super(escenario, posModoSeparacion , duracionModoAzul , velocidad, puntosAlSerComido);
 		this.arrasando = false;
 		this.casillasArrasadas = 0;
 	}
 
 
 	public void estrategizar() {
+		Random generadorRandom;
+		int	random =1;
+		
 		if (this.arrasando)
 			this.arrasar();
 		else{
-			random = new Random();
-			random.nextInt (10);
+			generadorRandom = new Random();
+			generadorRandom.nextInt (10);
 			if ((random==1)&& (this.casillasArrasadas < 10)){
 				this.arrasando = true;
 				this.arrasar();	
@@ -32,25 +35,38 @@ public class FantasmaNaranja extends Fantasma {
 	private void arrasar(){
 		Ueb casilleroActual;
 		NoJugador comestible;
+		Iterator<NoJugador> iteradorCasillero;
+		
 		if (this.casillasArrasadas >= 10){
 			this.arrasando = false;
 			return;
 		}
 		else{	
 			this.movimientoAlAzar();
-			casilleroActual = this.escenario.sacarEnPosicion(this.getPosicion());
-			casilleroActual.vaciarCasillero();
-			/* El tema de acá es ver bien como manejar el tema de los puntos
-			 * de las píldoras, y el pacman  debe chequear primero la casilla
-			 * porque sino puede pasar algo muy loco, el fantasma saca todo de la 
-			 * lista y el pacman no tienen nada que activar.
-			 */
+			casilleroActual = this.getEscenario().sacarEnPosicion(this.getPosicion());
+			iteradorCasillero = casilleroActual.iterator();
+			while(iteradorCasillero.hasNext()){
+				comestible = casilleroActual.sacarComestible(0);
+				comestible.activar();
+			}
 			this.casillasArrasadas = this.casillasArrasadas + 1;
 		}
 
 	}
 	
 	private void perseguir (){
-		/*A continuar*/
+		Calculador calculador = this.getEscenario().calculador();
+		Posicion posicionPacman = this.getEscenario().getPacman().getPosicion();
+		Posicion posicionPivote = this.getPosicionModoSeparacion();
+		Posicion posicionActual = this.getPosicion();
+		
+		if(posicionActual.distanciaA(posicionPivote)< posicionActual.distanciaA(posicionPacman)){
+			this.moverHacia(calculador.DireccionHaciaMenorCaminoEntre(this.getPosicion(), posicionPivote));
+		}
+		else{
+			this.moverHacia(calculador.DireccionHaciaMenorCaminoEntre(this.getPosicion(), posicionPacman));
+		}
+			
+	
 	}
 }
