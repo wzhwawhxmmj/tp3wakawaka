abb9fbdd3d5d7864fd564e2bcc8bfcd01ec17ce9
smesel <smesel@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-19 01:54:15 +0000

    Punto - Agregué el método distanciaHasta que retorna el modulo de la distancia entre dos puntos/posiciones.
    FantasmaVerde
    - Terminé la estrategia doble del fantasma.
    
    Estrategia 1: Hacer cualquiera.
    Sigue a Pacman y cuando lo alcanza, si le pinta, lo mata, sino agarra para el otro lado.
    
    Estrategia 2: Interdimensional Warp
    Esto es una masa, si se dan las condiciones de que el contador de salto interdimensional llega a 0 y que el fantasma está a un radio de 30 unidades de pacman, se teletransporta a una posición válida cercana a él. Es un capo.

--- src/logica/FantasmaVerde.java
+++ src/logica/FantasmaVerde.java
@@ -1,14 +1,80 @@
 package logica;
 
 public class FantasmaVerde extends Fantasma {
-
+	
+	private static final int pasosHastaSalto = 30;
+	private static final int distanciaDelSalto = 15;
+	private static final int cero = 0;
+	
+	private int temporizadorHastaSalto;
+	
 	public FantasmaVerde(Escenario escenario, Posicion posModoSeparacion, float duracionModoAzul, float velocidad, int puntosAlSerComido) { 
 		super(escenario, posModoSeparacion, duracionModoAzul, velocidad, puntosAlSerComido);
+		
+		this.temporizadorHastaSalto = pasosHastaSalto;
 	}
-
-	@Override
+	
+	private boolean posicionValida(Posicion posicion) {
+		if (this.getPosicion().distanciaHasta(this.getEscenario().getPacman().getPosicion()) >= 2 )
+			if (this.getEscenario().sacarEnPosicion(posicion).isPisablePorIA())
+				return true;
+		
+		return false;
+	}
+	
+	private Posicion posicionCercanaAPacman(){
+		Posicion comparacionConPacman;
+		
+		for (int i = -5 ; i <= 5 ; i++) {
+			comparacionConPacman = new Posicion(this.getEscenario().getPacman().getPosicion().getx() + i,this.getEscenario().getPacman().getPosicion().gety());
+			if (this.posicionValida(comparacionConPacman))
+					return comparacionConPacman;
+		}
+		
+		for (int j = -5 ; j <= 5 ; j++) {
+			comparacionConPacman = new Posicion(this.getEscenario().getPacman().getPosicion().getx(), this.getEscenario().getPacman().getPosicion().gety() + j);
+			if (this.posicionValida(comparacionConPacman))
+				return comparacionConPacman;
+		}
+		return this.getPosicion();
+	}
+	
+	/*private void actuarTonto() {
+		Calculador calc = this.getEscenario().calculador();
+		if (!pacmanEstaCerca())
+			this.moverHacia(calc.DireccionHaciaMenorCaminoEntre(this.getPosicion(), this.getEscenario().getPacman().getPosicion()));
+		else
+			this.movimientoAlAzar();
+	}*/
+	
+	private void actuarTonto(){
+		int distanciaMinima = 2;
+		Calculador calc = this.getEscenario().calculador();
+		
+		if(this.getPosicion().distanciaHasta(this.getEscenario().getPacman().getPosicion()) == distanciaMinima)
+			this.moverHacia(calc.DireccionHaciaMenorCaminoEntre(this.getPosicion(), this.getEscenario().getPacman().getPosicion()));
+		else
+			this.movimientoAlAzar();
+	}
+	
+	private void resetContador(){
+		this.temporizadorHastaSalto = pasosHastaSalto;
+	}
+	
+	private void saltar(){
+		
+		if ( (this.getPosicion().distanciaHasta(this.getEscenario().getPacman().getPosicion()) > distanciaDelSalto) && (this.temporizadorHastaSalto == cero) ){
+			this.moverHacia(this.posicionCercanaAPacman());
+		}
+		
+		if (this.temporizadorHastaSalto == cero)
+			this.resetContador();
+	}
+	
 	public void estrategizar() {
-		// TODO Auto-generated method stub
+		this.temporizadorHastaSalto--;
+		this.actuarTonto();
+		this.saltar();
 		
 	}
 
