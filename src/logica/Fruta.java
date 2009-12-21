9dd2dc7fbde24b474c4a3df098dd56e5160789b0
smesel <smesel@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-21 02:13:31 +0000

    Borré EscenarioTest y PisoTest porque no aportaban nada. Eliminé "implements Posicionable" de todas las clases herederas de Entidad porque la interfaz ya está implementada ahí.

--- src/logica/Fruta.java
+++ src/logica/Fruta.java
@@ -1,6 +1,6 @@
 package logica;
 
-public class Fruta extends NoJugador implements ar.uba.fi.algo3.titiritero.Posicionable{
+public class Fruta extends NoJugador {
 
 	
 		private boolean comido;
@@ -22,13 +22,5 @@ public class Fruta extends NoJugador implements ar.uba.fi.algo3.titiritero.Posic
 			this.setComido(true);
 			this.getEscenario().restarPuntos(1);
 			return this.getPuntaje();
-		}
-		
-		public int getX() {
-			return this.getPosicion().getx();
-		}
-		
-		public int getY() {
-			return this.getPosicion().gety();
 		}
 }
--- src/logica/Puntito.java
+++ src/logica/Puntito.java
@@ -1,6 +1,6 @@
 package logica;
 
-public class Puntito extends NoJugador implements ar.uba.fi.algo3.titiritero.Posicionable {
+public class Puntito extends NoJugador {
 	
 	
 
@@ -27,13 +27,5 @@ public class Puntito extends NoJugador implements ar.uba.fi.algo3.titiritero.Pos
 
 		public boolean isComido() {
 			return comido;
-		}
-		
-		public int getX() {
-			return this.getPosicion().getx();
-		}
-		
-		public int getY() {
-			return this.getPosicion().gety();
 		}
 }
