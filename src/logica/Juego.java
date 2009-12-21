16073ca5d09af68575b0c8539498112e5c7d183d
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-21 00:03:09 +0000

    MOdificaciones de funcionamiento en Juego y ListaDeEscenarios

--- src/logica/Juego.java
+++ src/logica/Juego.java
@@ -1,14 +1,19 @@
 package logica;
 
 import java.awt.Color;
+import java.io.IOException;
 import java.util.Iterator;
 
 import vista.*;
 
 import ar.uba.fi.algo3.titiritero.ControladorJuego;
+import ar.uba.fi.algo3.titiritero.Dibujable;
+import ar.uba.fi.algo3.titiritero.ObjetoVivo;
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
+import ar.uba.fi.algo3.titiritero.vista.Ventana;
 
 
-public class Juego {
+public class Juego implements ObjetoVivo{
 	
 	private Pacman pacman;
 	private Fantasma[] arrayDeFantasmas;
@@ -17,22 +22,22 @@ public class Juego {
 	private long puntaje;
 	private int vidas;
 	private int nivelActual;
-	//A verificar 
-	//Private bool frutaComida;
 	ControladorJuego controladorJuego;
+	SuperficieDeDibujo superficieDeDibujo;
 	
 	
 	
-	public Juego (){
+	public Juego () throws IOException{
 		
 		this.puntaje = 0;
 		this.vidas = 3;
 		this.nivelActual = 1;
 		this.arrayDeFantasmas = new Fantasma[4];
-		this.listaDeEscenarios = new ListaDeEscenarios();
+		this.listaDeEscenarios = new ListaDeEscenarios("C:\\ArchivoConEscenarios.txt", this);
 		this.pacman = null;
 		this.escenarioActual = null;
-	
+		this.controladorJuego = new ControladorJuego();
+		this.superficieDeDibujo = new Ventana (this.controladorJuego);
 	}	
 	
 
@@ -52,53 +57,67 @@ public class Juego {
 	}
 	
 	
-	private void prepaparVista(){
-		controladorJuego = new ControladorJuego();
-		controladorJuego.agregarObjetoVivo(pacman);
+	private void agregarObjetosVivosAlControlador(){
+		this.controladorJuego.agregarObjetoVivo(pacman);
 		VistaPacman vistaPacman = new VistaPacman();
-		controladorJuego.agregarDibujable(vistaPacman);
+		this.controladorJuego.agregarDibujable(vistaPacman);
 		
 		VistaFantasma vistasFantasma[] = new VistaFantasma[arrayDeFantasmas.length];
 		final Color coloresFantasma[] = {Color.RED,Color.ORANGE,Color.GREEN,Color.PINK};
 		for(int i=0;i<arrayDeFantasmas.length;i++)
 	     	{ 
-			controladorJuego.agregarObjetoVivo(arrayDeFantasmas[i]);
+			this.controladorJuego.agregarObjetoVivo(arrayDeFantasmas[i]);
 		    vistasFantasma[i] = new VistaFantasma(coloresFantasma[i]);
-		    controladorJuego.agregarDibujable(vistasFantasma[i]);
+		    this.controladorJuego.agregarDibujable(vistasFantasma[i]);
 	     	}
-			
-		VistaPiso vistasPiso[] = new VistaPiso[99];
-		VistaPared vistasPared[] = new VistaPared[99];
-		VistaCasa vistasCasa[] = new VistaCasa[10];
-		VistaPildora[] Vistaspildora = new VistaPildora[4];
-		VistaPuntito[] VistasPunto = new VistaPuntito[99];
-
-		//como detecto donde poner cada uno? 
-		//Yo diria que Lista de Escenarios por cada cosa llame un metodo en juego
-	}
+		}
 	
 	
-	private void inicializarNivel (int nivel){
+	private void inicializarNivel (int nivel) throws IOException{
 		
 		Escenario escenario = listaDeEscenarios.getEscenario(nivel);
-		
+		this.controladorJuego.setSuperficieDeDibujo(this.superficieDeDibujo);
 		this.escenarioActual = escenario;
 		this.inicializarFantasmas (escenario);
 		this.pacman = new Pacman(escenario, escenario.getPosicionInicialPacman(),2 );
+		this.agregarObjetosVivosAlControlador();
+		this.controladorJuego.agregarObjetoVivo(this);
 		
 	}
 	
-	public void jugar(){
+	public void jugar() throws IOException{
 		while ((listaDeEscenarios.tieneSiguiente())&& (this.vidas != 0)){
 			this.inicializarNivel(this.nivelActual);
 			while (((this.escenarioActual.getPuntosRestantes())!= 0) && (this.vidas != 0)){
-				/*Si no lo estoy pensando mal, acá va todo lo que es el titiritero
-				 *y esas yerbas, Este sería el método príncipal, falta discutir
-				 *Como termina el juego. 
-				 */
+				this.controladorJuego.comenzarJuego();
 			}
 		}
 	}
+
+	
+	public void vivir (){
+		if ((this.escenarioActual.getPuntosRestantes()!= 0)&& (this.vidas > 0)){
+			this.ActualizarPuntajeJugador();
+			if (!this.pacman.estaVivo()){
+				this.RutinaCuandoElPacmanMuere();
+			}
+		}
+		else{
+			this.controladorJuego.detenerJuego();
+		}
+		
+	}
+	
+	private void RutinaCuandoElPacmanMuere() {
+		this.vidas = this.vidas -1;
+		this.pacman.setPosicion(this.escenarioActual.getPosicionInicialPacman());
+		for (int i= 0; i<this.arrayDeFantasmas.length; i++){
+			this.arrayDeFantasmas[i].setPosicion(this.escenarioActual.getPosicionCasa());
+		}
+		
+	}
+
+
 
 	public void ActualizarPuntajeJugador(){
 		this.puntaje = this.pacman.getPuntajeAcumulado();
@@ -110,5 +129,9 @@ public class Juego {
 			this.arrayDeFantasmas[i].volverAzul();
 		}
 	}
-
+	
+	
+	public ControladorJuego getControlador (){
+		return this.controladorJuego;
+	}
 }
