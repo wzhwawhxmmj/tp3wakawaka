14e2f91292e1f27f74c122c075063e12f6ec1f6e
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-21 13:01:23 +0000

    Vistas dinámicas, cambian de color y se borran como es debido. Chequear si les parece bien como lo hice, está redefinido el método dibujar.

--- trunk/src/vista/VistaFantasma.java
+++ trunk/src/vista/VistaFantasma.java
@@ -1,13 +1,41 @@
 package vista;
 
 import java.awt.Color;
+import java.awt.Graphics;
+
+import logica.Fantasma;
+
+import ar.uba.fi.algo3.titiritero.Posicionable;
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
 import ar.uba.fi.algo3.titiritero.vista.Cuadrado;
 
 public class VistaFantasma extends Cuadrado {
  
-	public VistaFantasma(Color color) {
+	private Fantasma fantasma;
+	private int ancho;
+	private int alto;
+	private Color color;
+	
+	public VistaFantasma(Color unColor, Posicionable posicionable) {
 		super(15,15);
+		this.color = unColor;
+		this.ancho = 15;
+		this.alto = 15;
+		this.fantasma= (Fantasma) posicionable;
+		this.setPosicionable(this.fantasma);
 		setColor(color);
+	}
+	
+	public void dibujar(SuperficieDeDibujo superfice) {
+		Graphics grafico = (Graphics)superfice.getBuffer();
+		if(this.fantasma.estaAzul())
+			this.setColor(Color.BLUE);
+		else
+			this.setColor(this.color);
+		
+		grafico.setColor(this.getColor());
+		grafico.fillRect(this.getPosicionable().getX(), this.getPosicionable().getY(),ancho,alto);
+		
 	}
 
 }
--- trunk/src/vista/VistaPildora.java
+++ trunk/src/vista/VistaPildora.java
@@ -1,13 +1,34 @@
 package vista;
 
 import java.awt.Color;
+import java.awt.Graphics;
+
+import logica.NoJugador;
+import logica.Pildora;
+
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
 
 public class VistaPildora extends ar.uba.fi.algo3.titiritero.vista.Circulo {
 	 
-	        public VistaPildora() {
-	                super(9);
-	                setColor(Color.GREEN);
-	        }
+	private int radio;
+	private Pildora pildora;
+	
+	public VistaPildora(NoJugador unaPildora) {
+		super(9);
+	    this.radio = 9;
+		this.pildora= (Pildora) unaPildora;
+	    this.setPosicionable(this.pildora);
+		setColor(Color.GREEN);
+    }
 
+	public void dibujar(SuperficieDeDibujo superfice) {
+		Graphics grafico = (Graphics)superfice.getBuffer();
+		if(!this.pildora.isComido())
+			this.setColor(Color.GREEN);
+		else
+			this.setColor(Color.GRAY);
+		grafico.setColor(this.getColor());
+		grafico.fillOval(getPosicionable().getX() , getPosicionable().getY(),this.radio,this.radio);
 
+	}
 }
