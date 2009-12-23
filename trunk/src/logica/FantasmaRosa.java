5e2109800e2f96ce2a0ce3d8a122702dc7e37733
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-23 23:10:24 +0000

    FantasmaRosa Que hermoso refactor

--- trunk/src/logica/FantasmaRosa.java
+++ trunk/src/logica/FantasmaRosa.java
@@ -4,13 +4,14 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class FantasmaRosa extends Fantasma {
-
+    final Direccion prioridadDeDireccionesPorDefectoUno[] = {Direccion.ARRIBA, Direccion.DERECHA, Direccion.ABAJO,  Direccion.IZQUIERDA};
+    final Direccion prioridadDeDireccionesPorDefectoDos[] = {Direccion.ABAJO, Direccion.IZQUIERDA, Direccion.ARRIBA,  Direccion.DERECHA};
+    final int multiplicadorPacman = 500;
 	
 
 	public FantasmaRosa(Escenario escenario, Posicion posModoSeparacion, float duracionModoAzul, float velocidad, int puntosAlSerComido) {
 		super(escenario, posModoSeparacion, duracionModoAzul, velocidad, puntosAlSerComido);
 	}
-
 
 	private boolean esPisable(Posicion posicion){
 		try{
@@ -21,6 +22,7 @@ public class FantasmaRosa extends Fantasma {
 		return false;
 		}
 	}
+	
 	private Posicion nuevaPosicionHacia(Direccion direccion, Posicion posicion){
 		Posicion posicionNueva = posicion.clonar(); 
 		posicionNueva.moverHacia(direccion);
@@ -39,10 +41,7 @@ public class FantasmaRosa extends Fantasma {
 			}
 		}
 
-	
-	@Override
-	public void estrategizar() {
-		// TODO Auto-generated method stub
+	private Posicion CalcularPosicion(){
 		Escenario escenario = this.getEscenario();
 		Iterator<NoJugador> iteradorCosas;
 		Iterator<Posicion> iteradorPosicionCasilleros;
@@ -50,7 +49,6 @@ public class FantasmaRosa extends Fantasma {
 		Posicion posisionCasillero;  
 		int Xtotal = 0, Ytotal = 0;
 		int cantidad = 0;
-		
 		
 		iteradorPosicionCasilleros = escenario.iterator();
         while(iteradorPosicionCasilleros.hasNext()){
@@ -65,24 +63,30 @@ public class FantasmaRosa extends Fantasma {
 		            	}
 		            }
 			    }
-        final Direccion prioridadDeDireccionesPorDefectoUno[] = {Direccion.ARRIBA, Direccion.DERECHA, Direccion.ABAJO,  Direccion.IZQUIERDA};
-        final Direccion prioridadDeDireccionesPorDefectoDos[] = {Direccion.ABAJO, Direccion.IZQUIERDA, Direccion.ARRIBA,  Direccion.DERECHA};
-        Direccion prioridadDeDireccionesPorDefecto[];
-        Random ran = new Random(); 
-        if(ran.nextInt(1)==1)prioridadDeDireccionesPorDefecto = prioridadDeDireccionesPorDefectoUno;
-        else prioridadDeDireccionesPorDefecto = prioridadDeDireccionesPorDefectoDos;
-        
-
-        
-        
-        final int multiplicadorPacman = 500;
+               
         Xtotal += multiplicadorPacman * escenario.getPacman().getPosicion().getx();
         Ytotal += multiplicadorPacman * escenario.getPacman().getPosicion().gety();
         cantidad += multiplicadorPacman;
+        return new Posicion(Xtotal/cantidad, Ytotal/cantidad);
+	}
+	
+	
+	private Direccion[] prioridadDeDirecciones(){
+		if((new Random()).nextInt(2)==1)return prioridadDeDireccionesPorDefectoUno;
+        else 							return prioridadDeDireccionesPorDefectoDos;
+	}
+	
+	
+	@Override
+	public void estrategizar() {
+		// TODO Auto-generated method stub
+		
+        Direccion direccionFinal;
+        Posicion destino;
         
-        Posicion destino = new Posicion(Xtotal/cantidad, Ytotal/cantidad);
-		Posicion destinoPosta = nuevaPosicionPisable(destino, prioridadDeDireccionesPorDefecto);
-		this.moverHacia(escenario.calculador().DireccionHaciaMenorCaminoEntre(this.getPosicion(), destinoPosta));
+		destino = nuevaPosicionPisable(CalcularPosicion(), prioridadDeDirecciones());
+		direccionFinal  = this.getEscenario().calculador().DireccionHaciaMenorCaminoEntre(this.getPosicion(), destino);
+		this.moverHacia(direccionFinal);
 		
 	}
 
