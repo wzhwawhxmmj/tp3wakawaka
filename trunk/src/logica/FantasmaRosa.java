805e11fc7754abbb286a5e60a9a1497ac61fc432
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-23 04:38:14 +0000

    Fantasma Rosa Ya funciona Pero asumo que los puntos tienen que morir al ser comidos. Van a tener que cambiar eso.

--- trunk/src/logica/FantasmaRosa.java
+++ trunk/src/logica/FantasmaRosa.java
@@ -1,12 +1,44 @@
 package logica;
 
 import java.util.Iterator;
+import java.util.Random;
 
 public class FantasmaRosa extends Fantasma {
+
+	
 
 	public FantasmaRosa(Escenario escenario, Posicion posModoSeparacion, float duracionModoAzul, float velocidad, int puntosAlSerComido) {
 		super(escenario, posModoSeparacion, duracionModoAzul, velocidad, puntosAlSerComido);
 	}
+
+
+	private boolean esPisable(Posicion posicion){
+		try{
+		Ueb casillero = this.getEscenario().getUeb(posicion);
+		return casillero.isPisablePorIA();
+		}
+		catch (PosicionIlegalException e){
+		return false;
+		}
+	}
+	private Posicion nuevaPosicionHacia(Direccion direccion, Posicion posicion){
+		Posicion posicionNueva = posicion.clonar(); 
+		posicionNueva.moverHacia(direccion);
+		return posicionNueva;
+	}
+
+	private Posicion nuevaPosicionPisable(Posicion posicion, Direccion[] prioridadDeDirecciones){
+		if(esPisable(posicion))return posicion;
+		else {
+			Posicion posicionNueva = posicion;
+			for(int i=0;i<(prioridadDeDirecciones.length);i++){
+			       posicionNueva = nuevaPosicionHacia(prioridadDeDirecciones[i], posicion);
+			       if(esPisable(posicionNueva))break;
+				   }
+			return nuevaPosicionPisable(posicionNueva, prioridadDeDirecciones);
+			}
+		}
+
 	
 	@Override
 	public void estrategizar() {
@@ -33,8 +65,24 @@ public class FantasmaRosa extends Fantasma {
 		            	}
 		            }
 			    }
-		
-		this.moverHacia(escenario.calculador().DireccionHaciaMenorCaminoEntre(this.getPosicion(), new Posicion(Xtotal/cantidad, Ytotal/cantidad)));
+        final Direccion prioridadDeDireccionesPorDefectoUno[] = {Direccion.ARRIBA, Direccion.DERECHA, Direccion.ABAJO,  Direccion.IZQUIERDA};
+        final Direccion prioridadDeDireccionesPorDefectoDos[] = {Direccion.ABAJO, Direccion.IZQUIERDA, Direccion.ARRIBA,  Direccion.DERECHA};
+        Direccion prioridadDeDireccionesPorDefecto[];
+        Random ran = new Random(); 
+        if(ran.nextInt(1)==1)prioridadDeDireccionesPorDefecto = prioridadDeDireccionesPorDefectoUno;
+        else prioridadDeDireccionesPorDefecto = prioridadDeDireccionesPorDefectoDos;
+        
+
+        
+        
+        final int multiplicadorPacman = 500;
+        Xtotal += multiplicadorPacman * escenario.getPacman().getPosicion().getx();
+        Ytotal += multiplicadorPacman * escenario.getPacman().getPosicion().gety();
+        cantidad += multiplicadorPacman;
+        
+        Posicion destino = new Posicion(Xtotal/cantidad, Ytotal/cantidad);
+		Posicion destinoPosta = nuevaPosicionPisable(destino, prioridadDeDireccionesPorDefecto);
+		this.moverHacia(escenario.calculador().DireccionHaciaMenorCaminoEntre(this.getPosicion(), destinoPosta));
 		
 	}
 
