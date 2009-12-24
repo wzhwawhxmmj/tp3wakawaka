152507e2f9d76de6ca31ca26dae1529bac0ebb77
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-24 02:53:43 +0000

    VistaPacman Ahora mueve la boca!!! Y tiene ojito!!! (falta perfeccionar)

--- trunk/src/vista/VistaPacman.java
+++ trunk/src/vista/VistaPacman.java
@@ -10,17 +10,17 @@ import ar.uba.fi.algo3.titiritero.Posicionable;
 import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
 
 public class VistaPacman extends ar.uba.fi.algo3.titiritero.vista.CirculoIncompleto {
-			private int tiempo; 
+			private int anguloBoca; 
 			private int anguloInicial;
 			private int anguloFinal; 
+			private int desfaseOjoX;
+			private int desfaseOjoY;
 			private Pacman pacman;
 
 	        public VistaPacman(Posicionable posicionable) {
 	        		super(15,45,270);
-	                tiempo = 0;
-	                
-	                anguloInicial = 45;
-	                anguloFinal = 270;
+	        		anguloBoca = 0;
+
 	                pacman = (Pacman) posicionable; 
 	                
 	                setColor(Color.YELLOW);
@@ -31,23 +31,33 @@ public class VistaPacman extends ar.uba.fi.algo3.titiritero.vista.CirculoIncompl
 	    		grafico.setColor(getColor());
 	    		switch(pacman.Direccion()){
 	    			case ARRIBA:
-	    				anguloInicial = 45;
-	    				anguloFinal = -270;
+	    				desfaseOjoX = 1;
+	    				desfaseOjoY = 5;
+	    				anguloInicial = 125;
 	    				break;
 	    			case ABAJO:
-	    				anguloInicial = 225;
-	    				anguloFinal = -270;
+	    				desfaseOjoX = 1;
+	    				desfaseOjoY = 3;
+	    				anguloInicial = 315;
 	    				break;
 	    			case DERECHA:
+	    				desfaseOjoX = 4;
+	    				desfaseOjoY = 2;
 	    				anguloInicial = 45;
-	    				anguloFinal = 270;
 	    				break;
 	    			case IZQUIERDA:
-	    				anguloInicial = 135;
-	    				anguloFinal = -270;
+	    				desfaseOjoX = 5;
+	    				desfaseOjoY = 2;
+	    				anguloInicial = 225;
 	    				break;
 	    			}
+				anguloFinal = 270 + anguloBoca;
+				anguloBoca += 30;
+				if(anguloBoca > 90)anguloBoca=0;
+				
 	    		grafico.fillArc(getPosicionable().getX(), getPosicionable().getY(), 15, 15, anguloInicial, anguloFinal);
+	    		grafico.setColor(Color.BLACK);
+	    		grafico.fillOval(getPosicionable().getX()+desfaseOjoX,getPosicionable().getY()+desfaseOjoY, 3, 3);
 	        	
 	        }
 }
