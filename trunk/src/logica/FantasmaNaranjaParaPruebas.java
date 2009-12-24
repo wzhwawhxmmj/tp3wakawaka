0b5594afc528e214e939e1709c0af809b0b09517
jasampietro <jasampietro@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-24 23:05:32 +0000

    Test Del FantasmaNaranja listo.

---
+++ trunk/src/logica/FantasmaNaranjaParaPruebas.java
@@ -0,0 +1,33 @@
+package logica;
+
+
+
+public class FantasmaNaranjaParaPruebas extends FantasmaNaranja {
+
+	public FantasmaNaranjaParaPruebas(Escenario escenario,
+			Posicion posModoSeparacion, int duracionModoAzul, float velocidad,
+			int puntosAlSerComido) {
+			super(escenario, posModoSeparacion, duracionModoAzul, velocidad,
+				puntosAlSerComido);
+	
+	}
+	
+	public void estrategizar(){
+		if (this.arrasando)
+			this.arrasar();
+		else
+			this.perseguir();
+	}
+	
+	public void activarPerseguir(){
+		this.arrasando = false;
+	}
+	
+	public void activarArrasar(){
+		this.arrasando = true;
+	}
+
+
+	
+
+}
