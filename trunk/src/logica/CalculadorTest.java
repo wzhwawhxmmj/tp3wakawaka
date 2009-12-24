7ca1feb757b5540c0da0b3ceba9312fe24a183c5
nahuel36gg <nahuel36gg@361ee29e-cfd4-11de-831f-8f3f77348021>
2009-12-24 01:02:26 +0000

    CalculadorTest Volvio a funcionar

--- trunk/src/logica/CalculadorTest.java
+++ trunk/src/logica/CalculadorTest.java
@@ -5,23 +5,34 @@ import junit.framework.TestCase;
 
 
 public class CalculadorTest extends TestCase {
+	final Direccion prioridadDeDireccionesPorDefecto[] = {Direccion.ARRIBA, Direccion.DERECHA, Direccion.ABAJO,  Direccion.IZQUIERDA};	
+	Escenario escenario;
 	
-	public Direccion DireccionMenorCamino(Posicion salida, Posicion llegada){
-		ListaDeEscenarios lista = new ListaDeEscenarios(); 
-			try {
-				lista.cargarEscenario("mapaSimple.txt",null);
-			} catch (IOException e) {
-				return Direccion.NINGUNA;
+	public void setUp(){
+		escenario = new Escenario();
+		for (int i = 0 ; i < 5 ; i++){
+			for (int j = 0 ; j < 5 ; j++) {
+				if ( (i > 0) || (j > 0) || (i < 4) || (j < 4)){
+					escenario.addUeb(new Posicion(i,j), new Piso());
+					escenario.getUeb(new Posicion(i,j)).addNoJugador(new Puntito(escenario, new Posicion(i,j), 50));
+				}
+				if ( (i == 0) || (j == 0) || (i == 4) || (j == 4))
+					escenario.addUeb(new Posicion(i,j), new Pared());
 			}
-		Calculador calc = new Calculador(lista.getEscenario(1)); 
-		Direccion[] direcciones = new Direccion[4];
-		direcciones[0] = Direccion.ARRIBA;
-		direcciones[1] = Direccion.DERECHA;
-		direcciones[2] = Direccion.ABAJO;
-		direcciones[3] = Direccion.IZQUIERDA;
-		return calc.DireccionHaciaMenorCaminoEntre(salida, llegada, direcciones);
+		}
+		escenario.addUeb(new Posicion(2,2), new Pared());
+		escenario.setEsquinaInferiorDerecha(new Posicion(3,3));
+	}
+	
+	private Direccion DireccionMenorCamino(Posicion salida, Posicion llegada){
+		return new Calculador(escenario).DireccionHaciaMenorCaminoEntre(salida, llegada, prioridadDeDireccionesPorDefecto);
 		}
 
+	private Posicion hacerPosicionPisable(Posicion posicion){
+		return (new Calculador(escenario)).nuevaPosicionPisable(posicion, prioridadDeDireccionesPorDefecto);
+		
+	}
+	
 	
 	public void testDireccionExacta(){
 		//hay dos caminos posibles, deberia priorizar el de la derecha
@@ -87,7 +98,8 @@ public class CalculadorTest extends TestCase {
 		//por la prioridad de direcciones elegida...
 		//el piso mas cercano deberia ser en (2,1) 
 		Posicion salida = new Posicion(1,1); 
-		Posicion llegada = new Posicion(2,2);
+		Posicion llegadaFalsa = new Posicion(2,2);
+		Posicion llegada = hacerPosicionPisable(llegadaFalsa);
 		
 		if(DireccionMenorCamino(salida,llegada) == Direccion.DERECHA)
 			assert(true);
@@ -96,7 +108,8 @@ public class CalculadorTest extends TestCase {
 	
 	public void testParedEsquina(){
 		Posicion salida = new Posicion(1,1); 
-		Posicion llegada = new Posicion(4,4);
+		Posicion llegadaFalsa = new Posicion(4,4);
+		Posicion llegada = hacerPosicionPisable(llegadaFalsa);
 		
 		if(DireccionMenorCamino(salida,llegada) == Direccion.DERECHA)
 			assert(true);
@@ -106,7 +119,8 @@ public class CalculadorTest extends TestCase {
 	public void testParedDerecha(){
 		//el punto mas cercano seria el (3,2)
 		Posicion salida = new Posicion(1,1); 
-		Posicion llegada = new Posicion(4,2);
+		Posicion llegadaFalsa = new Posicion(4,2);
+		Posicion llegada = hacerPosicionPisable(llegadaFalsa);
 		
 		if(DireccionMenorCamino(salida,llegada) == Direccion.DERECHA)
 			assert(true);
@@ -116,7 +130,8 @@ public class CalculadorTest extends TestCase {
 	public void testParedAbajo(){
 		//el punto mas cercano seria el (2,3)
 		Posicion salida = new Posicion(3,1); 
-		Posicion llegada = new Posicion(2,4);
+		Posicion llegadaFalsa = new Posicion(2,4);
+		Posicion llegada = hacerPosicionPisable(llegadaFalsa);
 		
 		if(DireccionMenorCamino(salida,llegada) == Direccion.ABAJO)
 			assert(true);
@@ -127,7 +142,8 @@ public class CalculadorTest extends TestCase {
 		//el punto mas cercano seria el (1,2)
 		//o sea que no avanzaria
 		Posicion salida = new Posicion(1,2); 
-		Posicion llegada = new Posicion(0,2);
+		Posicion llegadaFalsa = new Posicion(0,2);
+		Posicion llegada = hacerPosicionPisable(llegadaFalsa); 
 		
 		if(DireccionMenorCamino(salida,llegada) == Direccion.NINGUNA)
 			assert(true);
